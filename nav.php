9930e6012a4b2306d3df76bea10931cee1e68794
zzxjoanw <zzxjoanw@gmail.com>
2015-08-14 18:35:25 -0400

    custom 20-column grid

--- nav.php
+++ nav.php
@@ -1,15 +1,9 @@
-<div class="nav">
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <a href="">Let's Learn Irish</a>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-3 col-xs-3 about">About</div>
-            <div class="col-md-3 col-xs-3 events">Events</div>
-            <div class="col-md-3 col-xs-3 shop">Shop</div>
-            <div class="col-md-3 col-xs-3 contact">Contact</div>
-        </div>
+<div class="container-fluid nav">
+    <div class="row">
+        <div class="col-xs-20 col-sm-4 name">LLI</div>
+        <div class="col-xs-5 col-sm-4 about">About</div>
+        <div class="col-xs-5 col-sm-4 events">Events</div>
+        <div class="col-xs-5 col-sm-4 shop">Shop</div>
+        <div class="col-xs-5 col-sm-4 contact">Contact</div>
     </div>
 </div>
